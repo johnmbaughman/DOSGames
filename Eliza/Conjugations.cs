b9fbd449fb862cf4e362cc81046a2988ea7541cc
John M. Baughman <unknown-user@10.0.0.6>
2015-08-31 22:33:21 -0600

    Add files

--- Eliza/Conjugations.cs
+++ Eliza/Conjugations.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Eliza
 {
-	public class Conjugations
+	public class Conjugations : List<string>
 	{
 		public Conjugations ()
 		{
+			this.Add(" ARE ");
+			this.Add(" AM ");
+			this.Add(" WERE ");
+			this.Add(" WAS ");
+			this.Add(" YOU ");
+			this.Add(" I ");
+			this.Add(" YOUR");
+			this.Add(" MY ");
+			this.Add(" I'VE ");
+			this.Add(" YOU'VE ");
+			this.Add(" I'M ");
+			this.Add(" YOU'RE ");
+			this.Add(" ME ");
+			this.Add(" YOU ");
 		}
 	}
 }
--- Eliza/EndProgramKeyWords.cs
+++ Eliza/EndProgramKeyWords.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Eliza
 {
-	public class EndProgramKeyWords
+	public class EndProgramKeyWords :List<string>
 	{
 		public EndProgramKeyWords ()
 		{
